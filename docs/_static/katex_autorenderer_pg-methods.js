ef2a048b3439e800771fad5065cd7d8980d31868
justLars7D1 <Larsquaedvlieg@outlook.com>
2022-04-27 12:02:10 +0200

    New build

---
+++ docs/_static/katex_autorenderer_pg-methods.js
@@ -0,0 +1,19 @@
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+   
+    pseudocode.renderElement(
+    document.getElementById("11"), {
+         captionCount: 10 ,
+        
+    });
+ 
+    pseudocode.renderElement(
+    document.getElementById("12"), {
+         captionCount: 11 ,
+        
+    });
+
+});
